283fae734eec9a696ee6f2c5f8f5e08057c67e56
asif riad <asifriad15@gmail.com>
2022-03-03 13:55:50 +0600

    dynamic converted

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const API_key = `81fd1e3b2100ca45ba986663c67445a3`;
 
 const searchTemp = () => {
     const search_city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search_city}&appid=${API_key}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search_city}&appid=${API_key}&units=metric`
     console.log(url);
     fetch(url)
         .then(res => res.json())
@@ -14,6 +14,11 @@ const setInnerText = (id, text) => {
 
 const displayTem = (temparature) => {
     setInnerText('city', temparature.name)
-    // const cityName = document.getElementById('city').
+    setInnerText('temperature', temparature.main.temp)
+    setInnerText('weather', temparature.weather[0].main)
+    // set weather icon link 
+    const url = `http://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png`
+    const iconImg = document.getElementById('weather-icon');
+    iconImg.setAttribute('src', url);
     console.log(temparature);
 }
